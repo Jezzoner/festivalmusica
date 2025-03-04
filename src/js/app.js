de6af8f194a52bb0a5f70250f6f8c999b02e9732
Jesus Daniel Castro <jesusdaniel.castros@gmail.com>
2025-03-04 00:31:49 -0500

    Project done

--- src/js/app.js
+++ src/js/app.js
@@ -24,9 +24,13 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     for(let i = 1; i <= cantidad_imagenes; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen Galeria'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
+
 
         // Event handler
         imagen.onclick = function() {
@@ -39,9 +43,12 @@ function crearGaleria() {
 
 function mostarImagen(i) {
 
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'Imagen Galeria'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+`;
 
 
     // Generar Modal
